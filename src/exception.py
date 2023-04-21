2c3b7583c03f9f8d22cabcc883546a1f52a055a5
VIJAYBPAWAR <vijaypawar908@gmail.com>
2023-04-21 09:34:13 +0530

    Model Trainer

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-from logger import logging
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
@@ -20,6 +20,7 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message    
     
+'''
 if __name__=="__main__":
     logging.info("Logging has started")
     try:
@@ -27,3 +28,4 @@ if __name__=="__main__":
     except Exception as e:
         logging.info('Dicision by zero') 
         raise CustomException(e,sys)
+'''
